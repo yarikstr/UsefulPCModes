81f4a1f0e1c99d01f67bc3df954bcda8bd336bf6
YarikCool <yarikmaster007@gmail.com>
2022-08-06 20:33:11 +0300

    everything is up to date

--- src/js/script.js
+++ src/js/script.js
@@ -9,27 +9,10 @@ btn.addEventListener("click", function () {
 });
 
 function randomColor() {
-  const allDigits = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    0,
-  ];
+  const hex = ["A", "B", "C", "D", "E", "F", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   let newColor = "#";
   for (let i = 0; i < 6; i++) {
-    let randomDigit = allDigits[Math.floor(Math.random() * allDigits.length)];
+    let randomDigit = hex[Math.floor(Math.random() * hex.length)];
     newColor += randomDigit;
   }
   return newColor;
